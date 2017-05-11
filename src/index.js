e3cc176bffd880364a77dd091bbc5993985f7635
Dmitry Basov <dmbasov@gmail.com>
2017-05-12 00:11:35 +0700

    fix cases with empty entity filter

--- src/index.js
+++ src/index.js
@@ -75,7 +75,7 @@ const PopByAge = Vizabi.Tool.extend("PopByAge", {
       }
 
       const entities_geodomain = model.state.entities_geodomain;
-      entities_geodomain.skipFilter = model.state.entities.dim === entities_geodomain.dim ||
+      entities_geodomain.skipFilter = (model.state.entities.dim === entities_geodomain.dim && !!model.state.entities.getFilteredEntities().length) ||
         model.state.entities_side.dim === entities_geodomain.dim;
     }
   },
