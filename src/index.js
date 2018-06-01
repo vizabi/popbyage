1984d2e342e422f1b61082ac59e9ff8196594c1e
Angie the Fairytale <konzeptmeister@gmail.com>
2018-06-01 18:09:16 +0200

    Add UI model to treemenu

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ const PopByAge = Vizabi.Tool.extend("PopByAge", {
     }, {
       component: Vizabi.Component.get("treemenu"),
       placeholder: ".vzb-tool-treemenu",
-      model: ["state.marker", "state.time", "locale"]
+      model: ["state.marker", "state.time", "locale", "ui"]
     }, {
       component: Vizabi.Component.get("datanotes"),
       placeholder: ".vzb-tool-datanotes",
