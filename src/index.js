841a98e79516ca2d749b374cf79debea79b598aa
Angie <konzeptmeister@gmail.com>
2021-05-10 20:18:33 +0200

    Clean up index a bit

--- src/index.js
+++ src/index.js
@@ -13,9 +13,6 @@ import {
   versionInfo
 } from "VizabiSharedComponents";
 import { VizabiPopByAge } from "./component";
-import { observable } from "mobx";
-
-const VERSION_INFO = { version: __VERSION, build: __BUILD };
 
 export default class PopByAge extends BaseComponent {
 
@@ -79,35 +76,13 @@ export default class PopByAge extends BaseComponent {
       locale: new LocaleService(config.locale),
       layout: new LayoutService({placeholder: config.placeholder})
     };
-
-    //register locale service in the marker model
-    config.model.config.markers.popbyage.data.locale = observable({
-      get id() { return config.services.locale.id; }
-    });
     
     super(config);
   }
 }
-
-// PopByAge.DEFAULT_UI = {
-//   chart: {}
-// }
-
-
-  // validate(model) {
-  //   model = this.model || model;
-
-  //   this._super(model);
-
-  //   //validate on first model set only
-  //   if (!this.model) {
-  //     const entities_geodomain = model.state.entities_geodomain;
-  //     entities_geodomain.skipFilter = (model.state.entities.dim === entities_geodomain.dim || model.state.entities_side.dim === entities_geodomain.dim) && 
-  //       (Boolean(model.state.entities.getFilteredEntities().length) || !model.state.entities_side.skipFilter);
-  //   }
-  // }
 PopByAge.DEFAULT_UI = {
   chart: {
+    mode: "smallMultiples",
     opacityHighlightDim: 0.1,
     opacitySelectDim: 0.3,
     opacityRegular: 1,
@@ -117,30 +92,6 @@ PopByAge.DEFAULT_UI = {
     lockActive: true,
     lockNonSelected: 0
   },
-}
-
-PopByAge.default_model = {
-  state: {
-  },
-  ui: {
-    chart: {
-      mode: "smallMultiples",
-      stacked: true,
-      inpercent: false,
-      flipSides: true,
-      lockActive: true,
-      lockNonSelected: 0
-    },
-    "buttons": ["colors", "find", "lock", /*"side",*/ "inpercent", "moreoptions", "sidebarcollapse", "fullscreen"],
-    "dialogs": {
-      "popup": ["timedisplay", "colors", "find", /*"side",*/ "moreoptions"],
-      "sidebar": ["timedisplay", "colors", "find", "grouping"],
-      "moreoptions": ["opacity", "speed", "grouping", "colors", /*"side",*/ "presentation", "about"],
-      "dialog": {"find": {"panelMode": "show"}}
-    },
-    presentation: false
-  },
-  locale: { }
-}
+};
 
 PopByAge.versionInfo = { version: __VERSION, build: __BUILD, package: __PACKAGE_JSON_FIELDS, sharedComponents: versionInfo};
